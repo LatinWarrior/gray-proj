f34b298668a67599de70e4715723173ba631c372
Luis Blanco <luisblanco@Luiss-MacBook-Pro.local>
2022-06-26 21:21:31 -0500

    Working on Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
     return (
         <div className='App'>
-            <h1 className='text-3xl font-bold underline'>Hello, World!</h1>
+            <NavBar />
+            <h1 className='text-3xl font-bold'>Hello, World!</h1>
         </div>
     );
 }
